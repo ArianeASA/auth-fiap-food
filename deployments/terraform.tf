a0600c5f64f2db47a8ce090d5b7c4c640472fea3
ArianeAlves <ArianeAlves@gitlab.com>
2023-11-09 02:15:56 -0300

    test backend terraform

--- deployments/terraform.tf
+++ deployments/terraform.tf
@@ -15,3 +15,12 @@ terraform {
     region = "us-east-2"
   }
 }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraforms-bucket"
+    key    = "states-lambda/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
