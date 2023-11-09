5393bdba6b86ad05694d61ec4dff347c534ee1e5
ArianeAlves <ArianeAlves@gitlab.com>
2023-11-09 14:35:41 -0300

    backend

--- deployments/terraform.tf
+++ deployments/terraform.tf
@@ -19,11 +19,11 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "terraforms-bucket"
-    key    = "states-lambda/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
+#data "terraform_remote_state" "network" {
+#  backend = "s3"
+#  config = {
+#    bucket = "terraforms-bucket"
+#    key    = "states-lambda/terraform.tfstate"
+#    region = "us-east-2"
+#  }
+#}
