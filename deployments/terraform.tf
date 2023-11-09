6c2168d5648ff217154b8e928db5e19ee0ccbfbf
ArianeAlves <ArianeAlves@gitlab.com>
2023-11-09 14:48:17 -0300

    clean deployment.yml

--- deployments/terraform.tf
+++ deployments/terraform.tf
@@ -13,17 +13,6 @@ terraform {
     bucket = "terraforms-bucket"
     key    = "states-lambda"
     region = "us-east-2"
-#    assume_role = {
-#      role_arn = "arn:aws:iam::860076335049:role/Terraform"
-#    }
   }
 }
 
-#data "terraform_remote_state" "network" {
-#  backend = "s3"
-#  config = {
-#    bucket = "terraforms-bucket"
-#    key    = "states-lambda/terraform.tfstate"
-#    region = "us-east-2"
-#  }
-#}
