40d297fdbaad8da5be1c6079a0e2382b03afbdd6
ArianeAlves <ArianeAlves@gitlab.com>
2023-11-09 21:10:20 -0300

    two jobs

--- deployments/s3_bucket.tf
+++ deployments/s3_bucket.tf
@@ -34,7 +34,8 @@ resource "aws_s3_object" "lambda_main" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
   key    = "main.zip"
-  source = local.output_path_zip
+#  source = local.output_path_zip
+  source =  "testV0.0.1/${local.output_path_zip}"
   acl   = "private"
   etag = data.archive_file.lambda_zip.output_base64sha256
 }
